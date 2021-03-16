b22263f9c11d78351f5647a4410f5521165d18ce
Dragan Jović <dragan_jovic1221@outlook.com>
2021-03-16 23:30:15 +0100

    komentar
    obrisan komentar

--- PrimeActivity/index.ts
+++ PrimeActivity/index.ts
@@ -1,15 +1,4 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
-
-import { AzureFunction, Context } from "@azure/functions"
+﻿import { AzureFunction, Context } from "@azure/functions"
 
 function isPrime(n: number): boolean {
     if (n <= 1)
--- PrimeOrchestrator/index.ts
+++ PrimeOrchestrator/index.ts
@@ -1,15 +1,4 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an HTTP starter function.
- * 
- * Before running this sample, please:
- * - create a Durable activity function (default name is "Hello")
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your 
- *    function app in Kudu
- */
-
-import * as df from "durable-functions"
+﻿import * as df from "durable-functions"
 
 interface Range {
     from: number;
@@ -95,7 +84,6 @@ function splitRanges(ranges: Range[], maxLength: number): Range[] {
     }
     return fixed;
 }
-
 
 const orchestrator = df.orchestrator(function* (context) {
     const activityName = "PrimeActivity";
